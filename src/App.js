d4f76854cd3469671483c8cfe177889712c49d17
safwat fathi <safwat.fathi@cultiv.llc>
2020-11-16 14:24:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import { EDITOR_JS_TOOLS } from "./constants";
+import EditorJs from "react-editor-js";
+import DragDrop from "editorjs-drag-drop";
+import Undo from "editorjs-undo";
 
-function App() {
+const App = () => {
+  let editor = useRef(null);
+
+  const [savedData, setSavedData] = useState({});
+
+  const handleSave = async () => {
+    let savedData;
+    if (!editor.current) {
+      savedData = await editor.save();
+      return savedData;
+    }
+
+    savedData = await editor.current.instance.save();
+    return savedData;
+  };
+
+  const data = {
+    time: 0,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "header",
+          level: 1,
+        },
+      },
+    ],
+  };
+
+  console.log(savedData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EditorJs
+        ref={editor}
+        instanceRef={(instance) => (editor = instance)}
+        data={data}
+        tools={EDITOR_JS_TOOLS}
+        onReady={() => {
+          new DragDrop(editor);
+          const undo = new Undo({ editor });
+          undo.initialize(data);
+        }}
+        onChange={async () => {
+          handleSave()
+            .then((data) => {
+              setSavedData(data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+import CheckList from "@editorjs/checklist";
+import Header from "@editorjs/header";
+import DragDrop from "editorjs-drag-drop";
+import Table from "@editorjs/table";
+
+export const EDITOR_JS_TOOLS = {
+  checklist: CheckList,
+  header: Header,
+  table: Table,
+};
