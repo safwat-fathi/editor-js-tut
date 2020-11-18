0ff53bbf23cd6044507918cecb25f18e05c55c63
safwat fathi <safwat.fathi@cultiv.llc>
2020-11-18 16:58:43 +0200

    feat: image uploader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { EDITOR_JS_TOOLS } from "./constants";
 import EditorJs from "react-editor-js";
 import DragDrop from "editorjs-drag-drop";
@@ -7,7 +7,46 @@ import Undo from "editorjs-undo";
 const App = () => {
   let editor = useRef(null);
 
-  const [savedData, setSavedData] = useState({});
+  const [savedData, setSavedData] = useState({
+    time: 0,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "header",
+          level: 1,
+        },
+      },
+      {
+        type: "image",
+        data: {
+          file: {
+            url:
+              "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
+          },
+          caption: "Roadster // tesla.com",
+          withBorder: true,
+          withBackground: false,
+          stretched: false,
+        },
+      },
+      {
+        type: "linkTool",
+        data: {
+          link: "https://codex.so",
+          meta: {
+            title: "CodeX Team",
+            site_name: "CodeX",
+            description:
+              "Club of web-development, design and marketing. We build team learning how to build full-valued projects on the world market.",
+            image: {
+              url: "https://codex.so/public/app/img/meta_img.png",
+            },
+          },
+        },
+      },
+    ],
+  });
 
   const handleSave = async () => {
     let savedData;
@@ -20,37 +59,26 @@ const App = () => {
     return savedData;
   };
 
-  const data = {
-    time: 0,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "header",
-          level: 1,
-        },
-      },
-    ],
-  };
-
-  console.log(savedData);
+  // console.log(savedData);
 
   return (
     <div className="App">
+      <h1 style={{ textAlign: "center" }}>Welcome</h1>
       <EditorJs
         ref={editor}
         instanceRef={(instance) => (editor = instance)}
-        data={data}
+        data={savedData}
         tools={EDITOR_JS_TOOLS}
         onReady={() => {
           new DragDrop(editor);
           const undo = new Undo({ editor });
-          undo.initialize(data);
+          undo.initialize(savedData);
         }}
         onChange={async () => {
           handleSave()
             .then((data) => {
               setSavedData(data);
+              console.log(data);
             })
             .catch((err) => {
               console.log(err);
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,29 @@
 import CheckList from "@editorjs/checklist";
 import Header from "@editorjs/header";
-import DragDrop from "editorjs-drag-drop";
 import Table from "@editorjs/table";
+import Image from "@editorjs/image";
+import linkTool from "@editorjs/link";
 
 export const EDITOR_JS_TOOLS = {
-  checklist: CheckList,
+  checklist: {
+    class: CheckList,
+    inlineToolbar: true,
+  },
   header: Header,
   table: Table,
+  image: {
+    class: Image,
+    config: {
+      endpoints: {
+        byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+        byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+      },
+    },
+  },
+  linkTool: {
+    class: linkTool,
+    config: {
+      endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching
+    },
+  },
 };
